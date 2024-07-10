6c6c97efee3b08d378c1a93666d713abbbcef79f
FiNy-JavaCoder <nyvlt.filip@gmail.com>
2024-07-10 12:01:44 +0200

    Delete src/main directory

--- src/main/java/finalProject/data/Repositories/InsuredRepository.java
+++
@@ -1,7 +0,0 @@
-package finalProject.data.Repositories;
-
-import finalProject.data.entities.InsuredEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface InsuredRepository extends CrudRepository<InsuredEntity, Long> {
-}
